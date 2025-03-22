bc3a7deb24de90bb0528962a53b30a2360c21482
RomiFalcon <145233427+RomiFalcon@users.noreply.github.com>
2025-03-22 01:09:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,9 @@ def get_transcript():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
